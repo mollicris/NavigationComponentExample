09db93d680b28c8441493ea654d8f39cda056dd2
crmollinedo <cristianm184@hotmail.com>
2022-09-19 23:37:00 -0400

    Add Project Navigation Component

---
+++ app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.anushka.navdemo1.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var binding:FragmentHomeBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment2_to_secondFragment2)
+        }
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/anushka/navdemo1/SecondFragment.kt
@@ -0,0 +1,36 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.anushka.navdemo1.databinding.FragmentSecondBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var binding:FragmentSecondBinding
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+        return  binding.root
+    }
+
+
+}
